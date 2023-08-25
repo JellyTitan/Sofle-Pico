c81928122dbe5572a3e365f5c4ef25334dbbc7a7
Ryan Neff <ryan.ray.neff@fluke.com>
2023-08-24 18:53:26 -0700

    Added firmware for v3.03 with working OLEDS

---
+++ qmk/sofle_v303/rules.mk
@@ -0,0 +1,13 @@
+# Split Keyboard Stuff
+SPLIT_KEYBOARD = yes
+SERIAL_DRIVER = vendor
+
+# Enable Bootmagic Lite
+BOOTMAGIC_ENABLE = yes
+
+# Default Folder
+DEFAULT_FOLDER = sofle_v303/rev1
+
+# @todo: Move to json
+OLED_DRIVER = SSD1306
+OLED_TRANSPORT = i2c
